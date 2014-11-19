533b9be2ea11f71bb3392e42620a1cb4dd99f489
Adam Barnhard <admbarn@gmail.com>
2014-11-18 22:21:24 -0600

    serving index page from public directory

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = [
-    {method: 'GET',  path: '/',         config: require('../controllers/home/get_home')},
     {method: 'GET',  path: '/{params*}', config: require('../controllers/static/get_statics')},
     {method: 'POST', path: '/register', config: require('../controllers/users/post_register')}
 ];
--- test/scripts/curl-tests.sh
+++ test/scripts/curl-tests.sh
@@ -3,7 +3,7 @@
 echo -e "\nhome page"
 curl -s 127.0.0.1:3000
 
-echo -e "\ncreate priority"
+echo -e "\ncreate user"
 curl -s -X POST -H "Content-Type: application/json" -d '{"name":"test", "password":"1234", "email":"bob@aol.com"}' 127.0.0.1:3000/register
 
 echo -e "\ntest static route"
