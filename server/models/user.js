00cbf97de83bcc4dd8733e93d10cdf29fc23c0bf
Adam Barnhard <admbarn@gmail.com>
2014-11-18 21:09:25 -0600

    ready to test user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,31 @@
 'use strict';
 
-var mongoose = require('mongoose'),
+var mongoose   = require('mongoose'),
+    bcrypt     = require('bcrypt'),
     UserSchema = new mongoose.Schema({
-        name: {type: String, required: true, validate: [nameV, 'name length']},
-        email: {type: String, required: true},
+        name:     {type: String, required: true, validate: [nameV, 'name length']},
+        email:    {type: String, required: true},
         password: {type: String, required: true},
-        pic:  {type: String, default: ''}
+        bio:      {type: String, default: 'I love pictures. Pictures are the best. Growing up I wanted to be either a picture or Batman. Now I just want to be a picture OF Batman.'},
+        pic:      {type: String, default: '/assets/default-profile.jpg'}
     });
 
-module.exports = mongoose.model('Task', {
-    name:       {type: String, required: true, validate: [nameV, 'name length']},
-    dueDate:    {type: Date, required: true},
-    isComplete: {type: Boolean, required: true},
-    priority:   {type: mongoose.Schema.ObjectId, ref: 'Priority', required: true}
-});
+UserSchema.statics.registerUser = function(o, cb){
+    this.findOne({email: o.email}, function(err, user){
+        if(user || o.password.length < 3 || err){return cb('Failed to register user' + err.toString());}
+        o.password = bcrypt.hashSync(o.password, 10);
+        user = new this(o);
+        user.save(function(err){
+            cb(err, user);
+        });
+    }.bind(this));
+};
 
 function nameV(v){
     return v.length > 0;
 }
+
+module.exports = mongoose.model('User', UserSchema);
 
 
 
