4063550e81bb00e6fe32530cff9aa16f15179b04
Logan Richardson <g.logan.richardson@gmail.com>
2014-11-18 21:26:17 -0600

    added tree for client

---
+++ client/views/crawl/crawl.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict';
+
+    angular.module('misceo')
+        .controller('CrawlCtrl', ['$scope', '$state', '$http', 'User', 'Crawl', '$location', function($scope, $state, $http, User, Crawl, $location){
+
+        }]);
+})();
