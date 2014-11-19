ea872c4fe2368a6b78a6daa638c03ad6584fa3d5
Logan Richardson <g.logan.richardson@gmail.com>
2014-11-18 21:44:34 -0600

    added tree for client

--- server/lib/security.js
+++ server/lib/security.js
@@ -4,12 +4,12 @@ var Bcrypt = require('bcrypt'),
     User   = require('../models/user');
 
 module.exports = function(username, password, callback){
-    var user = users[username];
+    var user = user[username];
     if(!user){
         return callback(null, false);
     }
 
     Bcrypt.compare(password, user.password, function(err, isValid){
-        callback(err, isValid, { id: user.id, name: user.name });
+        callback(err, isValid, {id: user.id, name: user.name});
     });
 };
