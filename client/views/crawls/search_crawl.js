441092b292128bb5639dc7da0a411249b3a07ffc
Logan Richardson <g.logan.richardson@gmail.com>
2014-11-20 14:05:33 -0600

    There are unused parameters, but the auth is working with the frontend

--- client/views/crawls/search_crawl.js
+++ client/views/crawls/search_crawl.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('misceo')
-  .controller('SearchCrawlCtrl', ['$scope', '$state', '$http', /*'User', */'$location', function($scope, $state, $http, /*User, */$location){
+  .controller('SearchCrawlCtrl', ['$scope', '$state', '$http', 'User', '$location', function($scope, $state, $http, User, $location){
     $scope.mode = $state.current.name;
 
   }]);
