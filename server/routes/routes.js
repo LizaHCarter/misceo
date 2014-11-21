9b59a14957aa220bb3e8101f2df76d99917e4416
Mikey Badr <mfbadr@gmail.com>
2014-11-21 10:01:25 -0600

    linting

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,4 @@
-'use strict';  module.exports = [ {method: 'GET',  path: '/{params*}', config: require('./config/static/get_statics')},
+'use strict';  module.exports = [{method: 'GET',  path: '/{params*}', config: require('./config/static/get_statics')},
     {method: 'GET'   , path: '/profile'  , config: require('./config/users/get_profile')},
     {method: 'PUT'   , path: '/profile'  , config: require('./config/users/put_profile')},
     {method: 'POST'  , path: '/register'  , config: require('./config/users/post_register')},
