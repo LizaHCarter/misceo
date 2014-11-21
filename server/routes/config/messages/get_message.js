d26548ddd9ee6621e5aacd7564331abeb93d0fb6
Mikey Badr <mfbadr@gmail.com>
2014-11-21 10:00:27 -0600

    added single message retrieval

---
+++ server/routes/config/messages/get_message.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Message = require('../../../models/messages'),
+    Joi = require('joi');
+
+module.exports = {
+    description: 'retrieve one message',
+    notes: 'A get to /messages/{messageId} should return a single message',
+    tags: ['messageing'],
+    validate: {
+        payload: {
+            messageId: Joi.string().length(24).required()
+        }
+    },
+    auth: {
+        mode: 'required'
+    },
+    handler: function(request, reply){
+        Message.oneMessage(request.params.messageId, function(err, message){
+            if(message){
+                reply(message).code(200);
+            }else{
+                reply(err).code(400);
+            }
+        });
+    }
+};
+
