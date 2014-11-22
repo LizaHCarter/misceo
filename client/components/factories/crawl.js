11cb507cbbde68117b6dea46bdba29b763a7faba
Adam Barnhard <admbarn@gmail.com>
2014-11-22 12:23:22 -0600

    added crawl factory

--- client/components/factories/crawl.js
+++ client/components/factories/crawl.js
@@ -1,3 +1,24 @@
-/**
- * Created by adambarnhard on 11/22/14.
- */
+(function(){
+    'use strict';
+
+    angular.module('misceo')
+        .factory('Crawl', ['$http', function($http){
+            function create(data){
+                return $http.post('/crawls', data);
+            }
+
+            function findAllForUser(){
+                return $http.get('/crawls');
+            }
+
+            function findOne(crawlId){
+                return $http.get('/crawls/' + crawlId);
+            }
+
+            function remove(crawlId){
+                return $http.delete('/crawls/' + crawlId);
+            }
+
+            return {create: create, findAllForUser: findAllForUser, findOne: findOne, remove: remove};
+        }]);
+})();
