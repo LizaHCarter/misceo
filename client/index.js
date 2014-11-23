f21453430ef3b2e5a468ac534fa09b4410c3a598
Mikey Badr <mfbadr@gmail.com>
2014-11-23 10:27:48 -0600

    spacing

--- client/index.js
+++ client/index.js
@@ -6,18 +6,18 @@
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-    .state('home',     {url:'/',         templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
+    .state('home',         {url:'/',         templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     //- the dashboard state is for testing ability to change states from home to logged in user views
-    .state('dashboard',{url:'/dashboard',templateUrl:'/views/dashboard/dashboard.html', controller:'DashboardCtrl'})
-    .state('profile',  {url:'/profile',  templateUrl:'/views/profile/profile.html', controller:'ProfileCtrl'})
-    .state('crawl',    {url:'/crawl',   templateUrl:'/views/crawls/search_crawl.html', controller:'SearchCrawlCtrl'})
-    .state('register', {url:'/register', templateUrl:'/views/users/users.html', controller:'UsersCtrl'})
+    .state('dashboard',    {url:'/dashboard',templateUrl:'/views/dashboard/dashboard.html', controller:'DashboardCtrl'})
+    .state('profile',      {url:'/profile',  templateUrl:'/views/profile/profile.html', controller:'ProfileCtrl'})
+    .state('crawl',        {url:'/crawl',   templateUrl:'/views/crawls/search_crawl.html', controller:'SearchCrawlCtrl'})
+    .state('register',     {url:'/register', templateUrl:'/views/users/users.html', controller:'UsersCtrl'})
     .state('viewMessages', {url:'/messages', templateUrl:'/views/messages/view_messages.html', controller:'ViewMsgsCtrl'})
-    .state('viewMessage', {url:'/messages/{messageId}', templateUrl:'/views/messages/view_message.html', controller:'ViewMsgCtrl'})
-    .state('newMessage', {url:'/messages/new', templateUrl:'/views/messages/new_message.html', controller:'NewMsgCtrl'})
-    .state('webcam',   {url:'/webcam',   templateUrl:'/views/webcam/webcam.html', controller:'WebcamCtrl'})
-    .state('login',    {url:'/login',    templateUrl:'/views/users/users.html', controller:'UsersCtrl'})
-    .state('logout',   {url:'/logout',   template:'',                           controller:'UsersCtrl'});
+    .state('viewMessage',  {url:'/messages/{messageId}', templateUrl:'/views/messages/view_message.html', controller:'ViewMsgCtrl'})
+    .state('newMessage',   {url:'/messages/new', templateUrl:'/views/messages/new_message.html', controller:'NewMsgCtrl'})
+    .state('webcam',       {url:'/webcam',   templateUrl:'/views/webcam/webcam.html', controller:'WebcamCtrl'})
+    .state('login',        {url:'/login',    templateUrl:'/views/users/users.html', controller:'UsersCtrl'})
+    .state('logout',       {url:'/logout',   template:'',                           controller:'UsersCtrl'});
 
     $localForageProvider.config({name:'misceo', storeName:'cache', version:1.0});
     $httpProvider.interceptors.push('HttpInterceptor');
