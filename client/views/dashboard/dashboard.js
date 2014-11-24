d03369d399371e36da26c212796b7c8b0c26bd9e
Melanie L. Fryman <melanie@frymanet.com>
2014-11-23 22:02:46 -0600

    added styles to crawls; some profile styles, but not much

--- client/views/dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-
-    angular.module('misceo')
-        .controller('DashboardCtrl', ['$scope', '$state', function($scope, $state){
-            $scope.mode = $state.current.name;
-        }]);
-})();
